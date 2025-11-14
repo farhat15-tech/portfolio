fd450aea9678556f6fd30eb7dee69d278369c834
farhat15-tech <farhatshahin9023@gmail.com>
2025-11-14 22:33:20 +0530

    my portfolio

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+let tablinks = document.getElementsByClassName('tab-links');
+let tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab(tabname) {
+    for (const tablink of tablinks) {
+        tablink.classList.remove('active-link');
+    }
+    for (const tabcontent of tabcontents) {
+        tabcontent.classList.remove('active-tab');
+    }
+
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
